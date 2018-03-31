22bbd7330084670a5b90b2251d375df7bf4986db
tanu0611 <32664178+tanu0611@users.noreply.github.com>
2018-04-01 00:05:22 +0530

    Add files via upload

---
+++ Multilevel_Queue.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdbool.h>
+#inlcude<stdlib.h>
+typedef struct Process{
+	int process_id;
+	int start_time;
+	int finish_time;
+	bool completed_time;
+	int priority;
+	int burst_time;
+} Process ;
+void RoundRobin()
+{
+	
+}
+void Fcfs()
+{
+	
+}
+void Priority()
+{
+	
+}
+int main()
+{	
+	int no_of_pr
+	printf("Enter the number of processes \n");
+	scanf(%d,&no_of_pr);	
+}
