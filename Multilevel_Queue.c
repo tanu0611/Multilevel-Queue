2b11aa5a3c5add2494ab407931cf7aedab1c2b2d
tanu0611 <32664178+tanu0611@users.noreply.github.com>
2018-04-05 22:56:55 +0530

    Add files via upload

--- Multilevel_Queue.c
+++ Multilevel_Queue.c
@@ -1,21 +1,16 @@
 #include<stdio.h>
 #include<stdbool.h>
 #include<stdlib.h>
-#include<pthread.h>
-typedef struct Process{
+struct Process{
 	int process_id;
 	int finish_time;
-	bool completed_time;
+	int completed_time;
 	int arr_time;
+	int start_time;
 	int priority;
 	int burst_time;
-;
-} st_Process ;
-st_Process process[] ;	
-int wt[5],tat[5],av_tat,av_wt;
-
-
-void *RoundRobin()
+};
+void RoundRobin()
 {
 		int tq=4;
 		int i;
@@ -30,42 +25,61 @@ void *RoundRobin()
 		
 	
 }
-void *Fcfs()
+void Fcfs()
 {
-		
+	for(i=1;i<=n;i++)
+	{
+		if(process[i].)
+		}	
 }
-void *Priority()
+void Priority()
 {
 	
 }
 int main()
 {	
-	int n,i;
+	int n,i,rr=0,pr=0,fc=0;
+	int queueTimer=10,tq=4,timer=0;
 	printf("Enter the number of processes \n");
 	scanf("%d",&n);
-    
-   
-    for(i=1;i<=n;i++)
+	struct Process p[n];
+    for(i=0;i<n;i++)
     {
     	printf("PROCESS %d \n",i);
-    	printf("Enter the process id \n");
-    	scanf("%d",process[i].process_id);
-    	printf("Enter the priority\n");
-    	scanf("%d",process[i].priority);
-    	process[i].arr_time=0;
-    	process[i].finish_time=0;
-    	printf("Enter burst time\n");
-    	scanf("%d",process[i].burst_time);
-    	process[i].completed_time=false;
-    	
-	}
-	for(i=1;i<=n;i++)
-	{
-		if(process[i].priority==1||process[i].priority==2)
-		RoundRobin();
-		else if(process[i].priority==3||process[i].priority==4)
-		Fcfs();
+		p[i].process_id=i+1;
+		printf("Enter the Arrival Time\n");
+    	scanf("%d",p[i].arr_time);
+    	printf("Enter the priority from 1 to 5\n");
+    	scanf("%d",p[i].priority);
+    	if(p[i].priority==1)
+		rr+;
+		else if(p[i].priority==5)
+		fc++;
 		else 
-		Priority();
+		pr++;
+		printf("Enter burst time\n");
+    	scanf("%d",p[i].burst_time);	
+		p[i].start_time=-1;
+    	process[i].completed_time=0;
+	}
+	int rrid=0,prid=0,fcid=0;
+	struct Process roundqueue[rr],priorqueue[pr],fcfsqueue[fc];
+	for(i=0;i<n;i++)
+	{
+		if(p[i].priority==1)
+		{
+			roundqueue[rrid]=p[i];
+			rrid++;
+		}
+		else if(p[i].priority==5)
+		{
+			priorqueue[prid]=p[i];
+			prid++;
+		}
+		else
+		{
+			fcfsqueue[fcid]=p[i];
+			fcid++;
+		}
 	}
 }
